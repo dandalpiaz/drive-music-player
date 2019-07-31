d06b0c85f90b452e5de8c4dbb7971eb6c70d7429
Dan Dalpiaz <dpdalpiaz@gmail.com>
2019-07-30 21:22:03 -0500

    add caching to api functions

--- api.js
+++ api.js
@@ -1,87 +1,106 @@
 
 function findMusicFolder() {
-  gapi.client.drive.files.list({
-    'pageSize': 1,
-    'q' : " name contains 'my-music' and trashed = false ",
-    'fields': "nextPageToken, files(id, name)"
-  }).then(function(response) {
-    
-    var files = response.result.files;
-    if (files && files.length > 0) {
-      var file = files[0];
-      findAlbumFolders(file.id);
-    } else {
-      $('#instructions').show();
-      //alert('No music folder found.');
-    }
+  caches.open('my-cache').then((cache) => {
+    cache.match('/folder.json').then((response) => {
+      try {
+        return response.json(); // try to return cache
+      }
+      catch {
+        return gapi.client.drive.files.list({
+          'pageSize': 1,
+          'q' : " name contains 'my-music' and trashed = false ",
+          'fields': "nextPageToken, files(id, name)"
+        }).then(function(response) {
+          if (response.result.files && response.result.files.length > 0) {
+            caches.open('my-cache').then((cache) => { // add to cache
+              cache.put('/folder.json', new Response( JSON.stringify(response) ));
+            });
+          }
+          return response; // try to return API request
+        });
+      }
+    }).then(function(data) {
+      if (data.result.files && data.result.files.length > 0) {
+        var file = data.result.files[0];
+        findAlbumFolders(file.id);
+      } else {
+        $('#instructions').show();
+      }
+    });
   });
 }
 
 function findAlbumFolders(id) {
-  var albumquery = "'" + id + "'" + " in parents and mimeType contains 'application/vnd.google-apps.folder' and trashed = false ";
-  gapi.client.drive.files.list({
-    'pageSize': 1000,
-    'orderBy': 'name',
-    'q' : albumquery,
-    'fields': "nextPageToken, files(id, name)"
-  }).then(function(response) {
-    
-    var albums = response.result.files;
-    
-    for (var i = 0; i < albums.length; i++) {
-      $('#albums').append("<button class='album' data-album-id='" + albums[i].id + "' " + "data-album-name='" + albums[i].name + "'>" + "<img src='" + "placeholder.jpg" + "' />" + "<span>" + albums[i].name + "</span>" + "</button>");
-    }
+  caches.open('my-cache').then((cache) => {
+    cache.match('/albums.json').then((response) => {
+      try {
+        return response.json(); // try to return cache
+      }
+      catch {
+        console.log("caught");
+        var albumquery = "'" + id + "'" + " in parents and mimeType contains 'application/vnd.google-apps.folder' and trashed = false ";
+        return gapi.client.drive.files.list({
+          'pageSize': 1000,
+          'orderBy': 'name',
+          'q' : albumquery,
+          'fields': "nextPageToken, files(id, name)"
+        }).then(function(response) {
+          if (response.result.files && response.result.files.length > 0) {
+            caches.open('my-cache').then((cache) => { // add to cache
+              cache.put('/albums.json', new Response( JSON.stringify(response) ));
+            });
+          }
+          return response; // try to return API request
+        });
+      }
+    }).then(function(data) {
+      if (data.result.files && data.result.files.length > 0) {
+        var albums = data.result.files;
+        for (var i = 0; i < albums.length; i++) {
+          $('#albums').append("<button class='album' data-album-id='" + albums[i].id + "' " + "data-album-name='" + albums[i].name + "'>" + "<img src='" + "placeholder.jpg" + "' />" + "<span>" + albums[i].name + "</span>" + "</button>");
+        }
+        findArts();
+      } else {
+        $('#instructions').show();
+      }
+    });
+  });
+}
 
-    caches.open('my-cache').then((cache) => {
-      request = '/arts.json';
-      cache.match(request).then((response) => {
-        try {
-          return response.json();
-        }
-        catch(err) {
-          getArts();
-        }
-         
-      }).then(function(data) {
-        var arts = data;
+function findArts() {
+  caches.open('my-cache').then((cache) => {
+    cache.match('/arts.json').then((response) => {
+      try {
+        return response.json(); // try to return cache
+      }
+      catch {
+        console.log("caught");
+        var albumartquery = "mimeType contains 'image/' and trashed = false and name contains 'folder.jpg' ";
+        return gapi.client.drive.files.list({
+          'pageSize': 1000,
+          'q' : albumartquery,
+          'fields': "nextPageToken, files(id, name, webContentLink, parents)"
+        }).then(function(response) {
+          if (response.result.files && response.result.files.length > 0) {
+            caches.open('my-cache').then((cache) => { // add to cache
+              cache.put('/arts.json', new Response( JSON.stringify(response) ));
+            });
+          }
+          return response; // try to return API request
+        });
+      }
+    }).then(function(data) {
+      if (data.result.files && data.result.files.length > 0) {
+        var arts = data.result.files;
         for (var i = 0; i < arts.length; i++) {
           var artURL = arts[i].webContentLink;
           var parent = arts[i].parents[0];
           $(".album[data-album-id=" + parent + "] img").attr('src', artURL);
-          console.log("loaded from cache");
         }
-  
-      });
+      } else {
+        //$('#instructions').show();
+      }
     });
-
-  });
-
-  
-}
-
-function getArts() {
-  // get arts
-  var albumartquery = "mimeType contains 'image/' and trashed = false and name contains 'folder.jpg' ";
-  gapi.client.drive.files.list({
-    'pageSize': 1000,
-    'q' : albumartquery,
-    'fields': "nextPageToken, files(id, name, webContentLink, parents)"
-  }).then(function(response) {
-    
-    var arts = response.result.files;
-
-    caches.open('my-cache').then((cache) => {
-      console.log(arts);
-      cache.put('/arts.json', new Response( JSON.stringify(arts) ));
-    });
-    
-    for (var i = 0; i < arts.length; i++) {
-      var artURL = arts[i].webContentLink;
-      var parent = arts[i].parents[0];
-      $(".album[data-album-id=" + parent + "] img").attr('src', artURL);
-      console.log("nah");
-    }
-
   });
 }
 
